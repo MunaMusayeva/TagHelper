137de6c5e1b27dfb514438e0231e66d9a3d97602
musa_qo76 <musa_qo76@STHQ0123-05>
2025-01-21 16:54:15 +0400

    finish

---
+++ ActionResultsTutorial/Entities/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ActionResultsTutorial.Entities
+{
+    public class Employee
+    {
+        [Range(1,100)]
+        public int Id { get; set; }
+
+        [DisplayName("User Name")]
+        [Required]
+        public string Firstname { get; set; }
+
+        [DisplayName("User Surname")]
+        [Required]
+        public string Lastname { get; set; }
+
+        public int CityId { get; set; }
+    }
+}
---
+++ ActionResultsTutorial/Startup.cs
@@ -0,0 +1,72 @@
+using ActionResultsTutorial.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionResultsTutorial
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            //var connectionString = "Data Source=(localdb)\\ProjectModels;Initial Catalog=SchoolDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<SchoolDbContext>(opt =>
+            {
+                opt.UseSqlServer(connectionString);
+            });
+
+            //services.AddSingleton<>();
+            //services.AddScoped<>();
+            //services.AddTransient<>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
